0d3666d7b4084d76af5b90352ac3a9835bd4deb3
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-11 22:12:04 -0400

    added the functions and unit tests

--- src/sum_square_difference/core.clj
+++ src/sum_square_difference/core.clj
@@ -1,7 +1,27 @@
 (ns sum-square-difference.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn sum-of-squares [range]
+  (reduce +
+    (for [number range] (* number number))))
+
+(defn square-of-the-sum [range]
+   (let [sum (reduce + range)]
+     (* sum sum)))
+
+(defn difference [square-of-sum sum-of-squares]
+   (- square-of-sum sum-of-squares))
+
+(defn -main []
+   (let [first-hundred (range 1 101)
+         square-of-sum (square-of-the-sum first-hundred)
+         sum-of-squares (sum-of-squares first-hundred)
+         difference (difference square-of-sum sum-of-squares)]
+         (prn "The square of the sum for the first 100 natural numbers "
+              square-of-sum
+              "The sum of squares for the first 100 natural numbers is "
+              sum-of-squares
+              "The difference is "
+              difference
+         )))
--- test/sum_square_difference/core_test.clj
+++ test/sum_square_difference/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [sum-square-difference.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest sum-of-squares-1through10-385
+  (testing "Sum of squares for 1 through 10 numbers is 385"
+    (is (= (sum-of-squares (range 1 11)) 385))))
+
+(deftest square-of-the-sum-1through10-3025
+ (testing "Square of the sum for 1 through 10 is 3025"
+    (is (= (square-of-the-sum (range 1 11)) 3025))))
+
+ (deftest difference-3025and385-2640
+  (testing "3025 - 385 is 2640"
+    (is (= (difference 3025 385) 2640))))
+
+;(run-tests 'sum-square-difference.core-test)
